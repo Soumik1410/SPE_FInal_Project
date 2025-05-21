ac7e108287a86a1b8d1124ca6334702c5ef80a94
Soumik1410 <soumik1410code@gmail.com>
2025-05-21 13:17:49 +0530

    Final full pipeline flow changes

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # app.py
 
-import streamlit as st
+import sys
 import numpy as np
 from PIL import Image
 from tensorflow.keras.preprocessing.image import img_to_array
@@ -10,26 +10,27 @@ import logging
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s | %(levelname)s | %(name)s | %(message)s',
-    handlers=[logging.StreamHandler()]
+    handlers=[logging.StreamHandler(sys.stdout)]
 )
 
 logger = logging.getLogger("caption-app")
+logger.setLevel(logging.INFO)
 
+#logger.info("Application started")
 
-logger.info("Application started")
-
+import streamlit as st
 st.title("Image Caption Generator")
 st.write("Upload an image and get an AI-generated caption.")
 
 uploaded_file = st.file_uploader("Choose an image", type=["jpg", "jpeg", "png"])
 
-logger.info("Calling infer.py generate_caption")
 if uploaded_file is not None:
     image = Image.open(uploaded_file).convert("RGB")
     st.image(image, caption="Uploaded Image", use_column_width=True)
 
     if st.button("Generate Caption"):
         img_array = img_to_array(image.resize((224, 224)))
+        logger.info("Calling infer.py generate_caption")
         caption = generate_caption(img_array)
         st.success("**Generated Caption:**")
         st.markdown(f"*{caption}*")
